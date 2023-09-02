63fa8833dbc337a61d26fbc08d82ba5cc544dd5b
Edsu <edsoneurides0705@gmail.com>
2023-09-02 17:22:58 -0300

    Comunicação com Mock para vericar se o usuário pode realizar o pagamento

---
+++ MyPicPayAPI/Integration/IMockVerifyPaymentRefit.cs
@@ -0,0 +1,10 @@
+﻿using Refit;
+
+namespace SimplePicPay.Integration
+{
+    public interface IMockVerifyPaymentRefit
+    {
+        [Get("/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6")]
+        Task<string> VerifyPaymentRefit();
+    }
+} 
