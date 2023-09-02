6e1a33649a980ffb9459b4b9e38e1ebbf790f653
Edsu <edsoneurides0705@gmail.com>
2023-09-02 17:21:46 -0300

    Conexão com banco em memória

---
+++ MyPicPayAPI/Data/ConnectionContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimplePicPay.Helpers;
+using SimplePicPay.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace SimplePicPay.Data
+{
+    public class ConnectionContext : DbContext
+    {
+        public DbSet<UserModel> Users { get; set; }
+
+        public ConnectionContext(DbContextOptions<ConnectionContext> options) : base(options)
+        {
+        }
+    }
+}
