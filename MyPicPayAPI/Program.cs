eaec7e153eb63fff8e53de568dd6f27f7dba2705
Edsu <edsoneurides0705@gmail.com>
2023-09-02 17:29:45 -0300

    Instânciar interfaces e criar dados fixos para testes

--- MyPicPayAPI/Program.cs
+++ MyPicPayAPI/Program.cs
@@ -1,3 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Refit;
+using SimplePicPay.Data;
+using SimplePicPay.Helpers;
+using SimplePicPay.Integration;
+using SimplePicPay.Models;
+using SimplePicPay.Repository;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +15,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ConnectionContext>(options =>
+    options.UseInMemoryDatabase("InMemoryDb"));
+
+builder.Services.AddScoped<ISendEmail, SendEmail>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IMockVerifyPayment, MockVerifyPayment>();
+
+builder.Services.AddRefitClient<IMockVerifyPaymentRefit>().ConfigureHttpClient(c =>
+{
+    c.BaseAddress = new Uri("https://run.mocky.io");
+});
 
 var app = builder.Build();
 
@@ -21,5 +42,18 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Adicionar dados iniciais ao contexto do banco de dados em mem ria
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ConnectionContext>();
+
+    if (!dbContext.Users.Any())
+    {
+        dbContext.Users.Add(new UserModel { Name = "Edson", Type = UserType.Default, CPF = "123.123.123-22", Email = "edsoneurides0705@gmail.com", Password = "123", Balance = 1000 });
+        dbContext.Users.Add(new UserModel { Name = "Andreia", Type = UserType.Default, CPF = "123.123.123-87", Email = "edsoneurides0705@gmail.com", Password = "123", Balance = 1000 });
+        dbContext.SaveChanges();
+    }
+}
 
 app.Run();
