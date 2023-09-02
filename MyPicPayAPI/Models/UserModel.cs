f7e1b6cdeb6c785dfce02fcc85620f896103167f
Edsu <edsoneurides0705@gmail.com>
2023-09-02 17:21:26 -0300

    Modelo de usuário

---
+++ MyPicPayAPI/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using SimplePicPay.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimplePicPay.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public UserType Type { get; set; }
+        public string? CPF { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public double Balance { get; set; }
+    }
+}
