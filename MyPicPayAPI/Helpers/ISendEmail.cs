e93aa20af9ad7405c1770a0012efd8301cf5fa74
Edsu <edsoneurides0705@gmail.com>
2023-09-02 17:23:45 -0300

    Criação de método envio de email

---
+++ MyPicPayAPI/Helpers/ISendEmail.cs
@@ -0,0 +1,7 @@
+﻿namespace SimplePicPay.Helpers
+{
+    public interface ISendEmail
+    {
+        Task<bool> SendMail(string email, string subject, string message);
+    }
+}
---
+++ MyPicPayAPI/Helpers/SendEmail.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace SimplePicPay.Helpers
+{
+    public class SendEmail : ISendEmail
+    {
+        public async Task<bool> SendMail(string email, string subject, string message)
+        {
+            try
+            {
+                string mail = "<your-email>"; // Your valid email, preference for outlook email 😉
+                string pw = "<your-password"; // Password email 🤐
+
+                string host = "smtp-mail.outlook.com";
+                int port = 587;
+
+                MailMessage mailMessage = new MailMessage()
+                {
+                    From = new MailAddress(mail, "PicPay Simplificado")
+                };
+
+                mailMessage.To.Add(email);
+                mailMessage.Subject = subject;
+                mailMessage.Body = message;
+                mailMessage.IsBodyHtml = true;
+                mailMessage.Priority = MailPriority.High;
+
+                using (SmtpClient smtp = new SmtpClient(host, port))
+                {
+                    smtp.Credentials = new NetworkCredential(mail, pw);
+                    smtp.EnableSsl = true;
+
+                    smtp.Send(mailMessage);
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            
+        }
+    }
+}
