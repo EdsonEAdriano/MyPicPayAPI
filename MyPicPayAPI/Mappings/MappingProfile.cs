0bc062d3d07e34c8d3859eb36be4ff2931fc8abe
Edsu <edsoneurides0705@gmail.com>
2023-09-03 22:40:40 -0300

    Configurações para o mapeamento entre modelos de usuarios

---
+++ MyPicPayAPI/Mappings/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Newtonsoft.Json.Linq;
+using SimplePicPay.Helpers;
+using SimplePicPay.Models;
+using SimplePicPay.ViewModels;
+
+namespace SimplePicPay.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserModel, UserViewModel>()
+                .ForMember(dest => dest.Type, map => map.MapFrom(src => src.Type == UserType.Store ? "Lojista" : "Padrão"))
+                .ReverseMap();
+        }
+    }
+}
