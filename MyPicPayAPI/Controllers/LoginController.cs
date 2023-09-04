ae32f3e27699fd1f4ec5e2a771ded6b3d3bd8a6c
Edsu <edsoneurides0705@gmail.com>
2023-09-04 01:04:38 -0300

    Criação do controller Login para podermos realizar a autenticação (no momento apenas se autenticando com valores fixos)

---
+++ MyPicPayAPI/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimplePicPay.Services;
+
+namespace SimplePicPay.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Authentication(string username, string password)
+        {
+            if (username.ToUpper() != "ADMIN" || password != "1")
+            {
+                return BadRequest("Login inválido!");
+            }
+
+
+            var token = TokenService.GenerateToken(username, password);
+            return Ok(token);
+        }
+    }
+}
