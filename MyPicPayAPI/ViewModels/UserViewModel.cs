fc5c3e3604ceb7906486780e046cf474e6ec4b63
Edsu <edsoneurides0705@gmail.com>
2023-09-03 22:39:50 -0300

    View Model de usuários

---
+++ MyPicPayAPI/ViewModels/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using SimplePicPay.Helpers;
+
+namespace SimplePicPay.ViewModels
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; } 
+        public string? Name { get; set; }
+        public string? Type { get; set; }
+        public string? Email { get; set; }
+        public double Balance { get; set; }
+    }
+}
