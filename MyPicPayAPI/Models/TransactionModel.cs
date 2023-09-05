d996f227df511a4c6c6e2692ad2b81e159791ed9
Edson <edsoneurides0705@gmail.com>
2023-09-04 21:42:55 -0300

    Criação de modelo Transações

---
+++ MyPicPayAPI/Models/TransactionModel.cs
@@ -0,0 +1,17 @@
+﻿using SimplePicPay.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimplePicPay.Models
+{
+    public class TransactionModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public UserModel Payer { get; set; }
+        public UserModel Payee { get; set; }
+        public double Value { get; set; }
+        public TransactionStatus Status { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
