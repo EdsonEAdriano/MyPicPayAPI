237f227abdee89ad9d95c9af9c6ab7a97dfcc1e4
Edsu <edsoneurides0705@gmail.com>
2023-09-08 23:02:07 -0300

    Modelo View para transações

---
+++ MyPicPayAPI/ViewModels/TransactionViewModel.cs
@@ -0,0 +1,18 @@
+﻿using SimplePicPay.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using SimplePicPay.Helpers;
+
+namespace SimplePicPay.ViewModels
+{
+    public class TransactionViewModel
+    {
+        public int Id { get; set; }
+        public string PayerName { get; set; }
+        public string PayeeName { get; set; }
+        public double Value { get; set; }
+        public string Status { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
