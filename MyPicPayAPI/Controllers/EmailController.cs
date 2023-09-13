ad845e62b7480615070f30cc651b6769ac064b9b
Edsu <edsoneurides0705@gmail.com>
2023-09-13 09:02:14 -0300

    Controlador para informar seu email para usar como envio de email

---
+++ MyPicPayAPI/Controllers/EmailController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SimplePicPay.Models;
+
+namespace SimplePicPay.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmailController : ControllerBase
+    {
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public IActionResult Post([FromBody] LoginModel login)
+        {
+            CredentialToSendEmail.email = login.email;
+            CredentialToSendEmail.password = login.password;
+
+
+            return Ok("Credenciais cadastradas para envio de Email.");
+        }
+    }
+}
